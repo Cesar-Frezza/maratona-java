1aa2aa3701ede23a0f2597033c2d830625795c2a
Cesar-Frezza <cesarfrezzanv@outlook.com>
2022-06-12 16:31:42 -0300

    Commit com novos ajustes.

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais06.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais06.java
@@ -4,7 +4,7 @@ public class Aula05EstruturasCondicionais06 {
     public static void main(String[] args) {
         // EXERCICIO Imprima utilizando o Switch, os valores de 1 a 7, se é dia util ou fim de semana
         //Considerando o 1 como Domingo
-        byte dia = 15;
+        byte dia = 6;
         switch (dia){
             case 1:
             case 7:
